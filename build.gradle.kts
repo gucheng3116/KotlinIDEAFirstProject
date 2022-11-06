0abf96a0f99f09e1506a41aa2933919be9325389
gucheng <459843397@qq.com>
2022-11-06 14:11:49 +0800

    build success

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+
+plugins {
+    kotlin("multiplatform") version "1.6.21"
+}
+
+group = "com.gucheng"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
+kotlin {
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+    sourceSets {
+        val nativeMain by getting
+        val nativeTest by getting
+
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")            }
+        }
+    }
+}
---
+++ src/nativeMain/kotlin/main.kt
@@ -0,0 +1,10 @@
+import kotlinx.coroutines.*
+fun main() {
+    runBlocking {
+        launch {
+            delay(1000L)
+            print(" world!")
+        }
+        print("Hello")
+    }
+}
